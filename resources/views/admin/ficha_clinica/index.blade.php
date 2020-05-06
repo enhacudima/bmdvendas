fa4d36a15531a4ae5db8d341f1a0f91644550da6
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2020-05-06 20:05:09 +0200

    fix

--- resources/views/admin/ficha_clinica/index.blade.php
+++ resources/views/admin/ficha_clinica/index.blade.php
@@ -205,6 +205,7 @@
                         <div class="media">
                             <div >
                                 <div class="media-body" style="width: 40%;margin:20% " >
+                                  {{$fc->paciente}}
                                     @if(isset($fc->paciente->nome))
                                     <h4 class="media-heading user_name">Paciente: {{$fc->paciente->nome}}</h4>
                                     Caderneta: {{$fc->paciente->caderneta}}<br>
